3fa407a72fb70f28b60a5f479d89c8b400d66565
synhershko <synhershko@apache.org>
2013-08-02 12:57:11 +0300

    Blog module: admin editing enabled

--- NSemble.Modules.Blog/Admin/BlogAdminModule.cs
+++ NSemble.Modules.Blog/Admin/BlogAdminModule.cs
@@ -9,23 +9,23 @@ using Raven.Client;
 
 namespace NSemble.Modules.Blog.Admin
 {
-	public class BlogAdminModule : Core.Nancy.NSembleAdminModule
-	{
-		public BlogAdminModule(IDocumentSession session)
-			: base("Blog")
-		{
-		    Get["/"] = o =>
-		                   {
-		                       ViewBag.ModulePrefix = AreaRoutePrefix.TrimEnd('/');
-		                       return View["List", session.Query<BlogPost>().ToArray()];
-		                   };
+    public class BlogAdminModule : Core.Nancy.NSembleAdminModule
+    {
+        public BlogAdminModule(IDocumentSession session)
+            : base("Blog")
+        {
+            Get["/"] = o =>
+                           {
+                               ViewBag.ModulePrefix = AreaRoutePrefix.TrimEnd('/');
+                               return View["List", session.Query<BlogPost>().ToArray()];
+                           };
 
-		    Get["/post-new/"] = p => View["Edit", new BlogPost
-		                {
-		                    ContentType = DynamicContentType.Markdown,
-		                    AllowComments = true,
-		                    CurrentState = BlogPost.State.Draft,
-		                }];
+            Get["/post-new/"] = p => View["Edit", new BlogPost
+                        {
+                            ContentType = DynamicContentType.Markdown,
+                            AllowComments = true,
+                            CurrentState = BlogPost.State.Draft,
+                        }];
 
             Post["/post-new/"] = p =>
             {
@@ -46,15 +46,55 @@ namespace NSemble.Modules.Blog.Admin
                 string tags = Request.Form.TagsAsString;
                 if (!String.IsNullOrWhiteSpace(tags))
                 {
-                    post.Tags = tags.Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+                    post.Tags = tags.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
                 }
 
                 session.Store(post);
-				session.Store(new PostComments(), post.Id + "/comments");
+                session.Store(new PostComments(), post.Id + "/comments");
                 session.SaveChanges();
 
                 return Response.AsRedirect(post.ToUrl(AreaRoutePrefix.TrimEnd('/')));
             };
-		}
-	}
+
+
+            Get[@"/edit/(?<id>\d+)"] = p =>
+            {
+                var blogPost = session.Load<BlogPost>((int)p.id);
+                if (blogPost == null)
+                    return 404;
+
+                return View["Edit", blogPost];
+            };
+
+            Post[@"/edit/(?<id>\d+)"] = p =>
+            {
+                var blogPost = session.Load<BlogPost>((int)p.id);
+                if (blogPost == null)
+                    return 404;
+
+                var input = this.Bind<BlogPost>();
+
+                bool validated = true;
+                if (!validated)
+                {
+                    //ModelState.AddModelError("Id", "");
+                    return View["Edit", input];
+                }
+
+                blogPost.Title = input.Title;
+                blogPost.Content = input.Content;
+
+                string tags = Request.Form.TagsAsString;
+                if (!String.IsNullOrWhiteSpace(tags))
+                {
+                    blogPost.Tags = tags.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                blogPost.LastEditedAt = DateTimeOffset.UtcNow;
+
+                session.SaveChanges();
+
+                return Response.AsRedirect(input.ToUrl(AreaRoutePrefix.TrimEnd('/')));
+            };
+        }
+    }
 }
