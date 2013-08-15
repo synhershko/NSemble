714bd96de09d36e41c8858a3f1954488ced41327
synhershko <itamar@code972.com>
2013-08-15 14:51:50 +0100

    Blog module: tags now clickable

--- NSemble.Modules.Blog/Helpers/UrlHelpers.cs
+++ NSemble.Modules.Blog/Helpers/UrlHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using NSemble.Modules.Blog.Models;
+using Nancy.ViewEngines.Razor;
 
 namespace NSemble.Modules.Blog.Helpers
 {
@@ -16,6 +18,20 @@ namespace NSemble.Modules.Blog.Helpers
         {
             return string.Concat(prefix, "/", post.PublishedAt.Year, "/", post.PublishedAt.Month.ToString("D2"), "/",
                                  post.Id.Substring(post.Id.IndexOf("/", StringComparison.Ordinal) + 1), "-", post.Slug);
+        }
+
+        public static IHtmlString TagLinks(string prefix, IEnumerable<string> tags)
+        {
+            if (tags == null) return NonEncodedHtmlString.Empty;
+
+            var sb = new StringBuilder();
+            int i = 0;
+            foreach (var tag in tags)
+            {
+                if (i++ > 0) sb.Append(", ");
+                sb.AppendFormat(@"<a href=""{1}/tagged/{0}"">{0}</a>", tag, prefix);
+            }
+            return new NonEncodedHtmlString(sb.ToString());
         }
 
         public static string BlogUrl(string prefix, int? year, int? month, string tag, int? page)
