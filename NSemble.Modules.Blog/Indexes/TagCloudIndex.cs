72f179a42a54c0795317861b4eba5a0b172d15f7
Itamar Syn-Hershko <itamar@code972.com>
2014-08-01 00:13:27 +0300

    Blog: Adding TagCloud index

---
+++ NSemble.Modules.Blog/Indexes/TagCloudIndex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NSemble.Modules.Blog.Models;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace NSemble.Modules.Blog.Indexes
+{
+    public class TagCloudIndex : AbstractIndexCreationTask<BlogPost, TagCloudIndex.ReduceResult>
+    {
+        public class ReduceResult
+        {
+            public string Tag { get; set; }
+            public int Count { get; set; }
+            public DateTime LastPost { get; set; }
+        }
+
+        public TagCloudIndex()
+        {
+            Map = docs => from doc in docs
+                from tag in doc.Tags
+                select new {Tag = tag, TagAnalyzed = tag, Count = 1, LastPost = doc.PublishedAt};
+
+            Reduce = results => from r in results
+                group r by r.Tag.ToLowerInvariant()
+                into g
+                select
+                    new
+                    {
+                        Tag = g.Key,
+                        TagAnalyzed = g.Key,
+                        Count = g.Sum(x => x.Count),
+                        LastPost = g.Max(x => x.LastPost)
+                    };
+
+            Index("Tag", FieldIndexing.NotAnalyzed);
+            Index("TagAnalyzed", FieldIndexing.Analyzed);
+        }
+    }
+}
